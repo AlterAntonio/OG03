7551edf3739d08a1aa5ba5ab4079dce51219d557
Antonio <antonpogoryelov@gmail.com>
2024-06-18 20:28:53 +0300

    add_constants
    What was added: screens parameters, name & icon

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.setmode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Тир')
+icon = pygame.image.load('')
+
 running = True
 while running:
     pass
