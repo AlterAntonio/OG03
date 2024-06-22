f73ee5e1aedbc24c43e83a88c2165d41aae20ec8
Antonio <antonpogoryelov@gmail.com>
2024-06-22 12:22:47 +0300

    Cycle_ready
    Added cycle with target moving

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
 import pygame
 import random
 
+#Инициализация библиотек
 pygame.init()
 
+#Задание размеров окна
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-screen = pygame.display.setmode((SCREEN_WIDTH, SCREEN_HEIGHT))
+#И создание этого окна
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+#Название заголовка окна
 pygame.display.set_caption('Тир')
+#Определение локальных изображений для главного окна и мишени
 icon = pygame.image.load('img/duckhunt.jpg')
 target_img = pygame.image.load('img/duck.png')
+#Определение размеров и координат мишени
 target_width = 50
 target_height = 50
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
-target_у = random.randint(0, SCREEN_HEIGHT - target_height)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+#Задание цвета фона
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
+#Игровой цикл
 running = True
 while running:
-    pass
+    #Заполнение фона
+    screen.fill(color)
+    #Дальше идёт обработка событий
+    for event in pygame.event.get():
+        #Условие выхода из игры - закрытие окна
+        if event.type == pygame.quit: running = False
+        #Обработка нажатий кнопок мыши
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            #Считывание координат мыши и сравнение с положением мишени
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+    screen.blit(target_img, (target_x, target_y))
+    #Обновление экрана
+    pygame.display.update()
 
+
+#Завершение игры
 pygame.quit()
